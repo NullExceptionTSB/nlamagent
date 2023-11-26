c35612b17d4a8b3c8c87b5a0a84fcc96e69f2169
NullExceptionTSB <41920570+NullExceptionTSB@users.noreply.github.com>
2023-11-26 19:56:40 +0100

    init

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+#include <stdio.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                     LPSTR lpString, INT bShowCmd) {
+    DWORD a;
+
+    
+}
